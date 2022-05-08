c54beef84c2d1d78b0ed77556e053dedb497c86c
Florian Klampfer <mail@qwtel.com>
2022-05-08 11:22:02 +0700

    Change readable stream to be pull-based

--- index.ts
+++ index.ts
@@ -1,4 +1,20 @@
-export type ForOfAwaitable<T> = Iterable<T> | AsyncIterable<T>;
+export type ForAwaitable<T> = Iterable<T> | AsyncIterable<T>;
+export { ForAwaitable as ForOfAwaitable }
+
+export type ForAwaitableIterator<T> = AsyncIterator<T> | Iterator<T>
+
+const isIterable = <T>(x: unknown): x is Iterable<T> =>
+  x != null && typeof x === 'object' && Symbol.iterator in x
+
+const isAsyncIterable = <T>(x: unknown): x is AsyncIterable<T> =>
+  x != null && typeof x === 'object' && Symbol.asyncIterator in x
+
+const forAwaitableIterator = <T>(x: unknown): ForAwaitableIterator<T> =>
+  isAsyncIterable<T>(x)
+    ? x[Symbol.asyncIterator]()
+    : isIterable<T>(x) 
+      ? x[Symbol.iterator]()
+      : (() => { throw Error('Not for-awaitable. Neither Symbol.asyncIterator nor Symbol.iterator found.') })()
 
 export async function* streamToAsyncIterable<T>(stream: ReadableStream<T>): AsyncIterableIterator<T> {
   const reader = stream.getReader();
@@ -9,36 +25,40 @@ export async function* streamToAsyncIterable<T>(stream: ReadableStream<T>): Asyn
       ({ done, value } = await reader.read());
     }
     reader.releaseLock();
-  } catch (err) { 
+  } catch (err) {
     reader.cancel(err);
   }
 }
 
-function asyncIterableToStreamTS<T>(iterable: ForOfAwaitable<T>): ReadableStream<T> {
+// Older version of Cloudflare Workers do not support the `ReadableStream` constructor, 
+// so we use a TransformStream instead:
+function asyncIterableToStreamTS<T>(iterable: ForAwaitable<T>): ReadableStream<T> {
   const { readable, writable } = new TransformStream<T, T>();
   (async () => {
     const writer = writable.getWriter();
     try {
-      for await (const x of iterable) 
+      for await (const x of iterable)
         writer.write(x);
       writer.close();
-    } catch (err) { 
+    } catch (err) {
       writer.abort(err);
     }
   })();
   return readable;
 }
 
-function asyncIterableToStreamRS<T>(iterable: ForOfAwaitable<T>): ReadableStream<T> {
+function asyncIterableToStreamRS<T>(iterable: ForAwaitable<T>): ReadableStream<T> {
+  let iterator: ForAwaitableIterator<T>;
   return new ReadableStream({
+    start() {
+      iterator = forAwaitableIterator(iterable);
+    },
     async pull(controller) {
-      try {
-        for await (const x of iterable) 
-          controller.enqueue(x);
-        controller.close();
-      } catch (err) { 
-        controller.error(err) 
-      }
+      const { value, done } = await iterator.next();
+      if (!done) controller.enqueue(value); else controller.close();
+    },
+    async cancel(reason) {
+      try { await iterator.throw?.(reason) } catch { }
     },
   });
 }
@@ -47,8 +67,7 @@ const tryReadableStream = () => {
   try { return !!new ReadableStream({}) } catch { return false }
 };
 
-// Cloudflare Workers do not support the `ReadableStream` constructor:
-export const asyncIterableToStream: <T>(iterable: ForOfAwaitable<T>) => ReadableStream<T> = tryReadableStream()
+export const asyncIterableToStream: <T>(iterable: ForAwaitable<T>) => ReadableStream<T> = tryReadableStream()
   ? asyncIterableToStreamRS
   : asyncIterableToStreamTS;
 
